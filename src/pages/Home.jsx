db30df828ebb4a0ace261f16d50b753eb4618631
Kipkirui Keter <kipkiruiketer755@gmail.com>
2024-03-06 17:23:00 +0300

    login redirect on home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import data from "../utils/data.json"
 import NavBar from "../components/NavBar"
 import useFetch from "../hooks/useFetch"
 import BookCard from "../components/BookCard"
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom"
 import { useContext, useState, useEffect } from "react"
 import { AuthContext } from "../context/AuthContext"
 import coverimage from "../assets/coverimage.png"
@@ -21,16 +21,18 @@ const Home = () => {
             })
             const data = await response.json()
             setDiscoverBooks(data)
-        } catch(error){
+        } catch (error) {
             console.error(error)
         }
     }
-    useEffect(()=> {
+    useEffect(() => {
         fetchData()
-    },[url])
+    }, [url])
     console.log(discoverbooks)
-    console.log(session.access_token)
-    
+    if (session) {
+        console.log(session.access_token)
+    }
+
     return (
         <>
             {/* <NavBar />
@@ -51,22 +53,22 @@ const Home = () => {
                 <NavBar />
 
                 <div className="flex flex-col bg-wheat">
-                <h3 className="font-lexend text-4xl text-teal-900 my-10 ml-10">POPULAR</h3>
-                <div className="flex flex-wrap gap-4 md:px-16 mx-auto">
-                    {
-                        searchResults && searchResults.items && searchResults.items.map((book) => {
-                            return (
-                                <Link to={`/book/${book.id}`} state={book}><BookCard image={book.volumeInfo.imageLinks ? book.volumeInfo.imageLinks.thumbnail: coverimage} author={book.volumeInfo.authors ? book.volumeInfo.authors[0]: "Not found"} title={book.volumeInfo.title}/></Link>
-                            )
-                        })
-                    }
+                    <h3 className="font-lexend text-4xl text-teal-900 my-10 ml-10">POPULAR</h3>
+                    <div className="flex flex-wrap gap-4 md:px-16 mx-auto">
+                        {
+                            searchResults && searchResults.items && searchResults.items.map((book) => {
+                                return (
+                                    <Link to={`/book/${book.id}`} state={book}><BookCard image={book.volumeInfo.imageLinks ? book.volumeInfo.imageLinks.thumbnail : coverimage} author={book.volumeInfo.authors ? book.volumeInfo.authors[0] : "Not found"} title={book.volumeInfo.title} /></Link>
+                                )
+                            })
+                        }
+                    </div>
                 </div>
-            </div>
             </> : <>
-            <div>
-                <h1 className="font-lexend text-xl">Ooops....You aren't logged in</h1>
-                <Link to="/"><button className="flex justify-center items-center px-8 py-4 bg-teal-900 text-white font-semibold hover:bg-teal-700 rounded-full gap-x-5 my-10">Go Log In</button></Link>
-            </div>
+                <div>
+                    <h1 className="font-lexend text-xl">Ooops....You aren't logged in</h1>
+                    <Link to="/"><button className="flex justify-center items-center px-8 py-4 bg-teal-900 text-white font-semibold hover:bg-teal-700 rounded-full gap-x-5 my-10">Go Log In</button></Link>
+                </div>
             </>}
         </>
     )
