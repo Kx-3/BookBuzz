f0dfdbafae6af6e829b2dc059ca295cb85d7997d
Kipkirui Keter <kipkiruiketer755@gmail.com>
2024-03-07 09:43:45 +0300

    added sidebar

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -5,10 +5,10 @@ import { Link } from "react-router-dom"
 const SideBar = () => {
     return (
         <>
-            <div className=" w-1/5 left-0 h-2/3 bottom-0 bg-peach fixed flex flex-col items-center font-lexend">
-                <Link className="w-4/5 flesx justify-center hover:bg-wheat text-teal-900" to="/home">Home</Link>
-                <Link className="w-4/5 flesx justify-center hover:bg-wheat text-teal-900" to="/bestsellers">Bestsellers</Link>
-                <Link className="w-4/5 flesx justify-center hover:bg-wheat text-teal-900" to="">My Favorites</Link>
+            <div className=" w-1/5 left-0 h-screen bottom-0 bg-peach fixed flex flex-col items-center font-lexend">
+                <Link className="w-4/5 rounded-lg flex justify-center hover:bg-wheat text-teal-900" to="/home">Home</Link>
+                <Link className="w-4/5 rounded-lg flex justify-center hover:bg-wheat text-teal-900" to="/bestsellers">Bestsellers</Link>
+                <Link className="w-4/5 rounded-lg flex justify-center hover:bg-wheat text-teal-900" to="">My Favorites</Link>
             </div>
         </>
     )
